82cbd78e7c85d07cc9be35d9bc00635070e08903
kjy <2021112090@dgu.ac.kr>
2026-04-13 13:31:39 +0900

    Initial commit

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const ContactInfo = () => {
   return (
     <div className="contact-info">
-      <p>이메일: leesh914@naver.com</p>
-      <p>전화번호: 010-5559-****</p>
+      <p>이름: 김준영</p>
+      <p>이메일: 2021112090@dgu.ac.kr</p>
+      <p>전화번호: 010-3336-****</p>
       <p>주소: 동국대학교 신공학관</p>
     </div>
   );
